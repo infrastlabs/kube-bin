63dbbddafeaa100536170ff50c7c95dc9856763b
sam <huapox@126.com>
2019-09-14 16:26:25 +0800

    clone -b sam-v070

--- k3s/build.sh
+++ k3s/build.sh
@@ -2,7 +2,7 @@
 set +e
 
 dir=/go/src/github.com/rancher/k3s/ && mkdir -p $dir
-    git clone https://github.com/huapox/fk-k3s-custom $dir
+    git clone -b sam-v070  https://github.com/huapox/fk-k3s-custom $dir
 
 cd $dir && ls -lh && git branch -a
 cd scripts
